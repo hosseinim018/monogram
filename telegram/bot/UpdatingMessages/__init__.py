9dbb9121c60cfb8d8a154b37820166c655d8d2fb
mahdi hosseini <hosseinim018@gmail.com>
2023-09-06 00:40:46 +0330

    add all method to __init__

--- telegram/bot/UpdatingMessages/__init__.py
+++ telegram/bot/UpdatingMessages/__init__.py
@@ -1,1 +1,19 @@
- 
+from deleteMessage import deleteMessage
+from editMessageCaption import editMessageCaption
+from editMessageText import edit_message
+from stopMessageLiveLocation import stop_message_live_location
+from stopPoll import stop_poll
+from editMessageLiveLocation import edit_message_live_location
+from editMessageMedia import edit_message_media
+from editMessageReplyMarkup import edit_message_reply_markup
+
+__All__ = [
+    'deleteMessage',
+    'editMessageCaption',
+    'editMessageText',
+    'editMessageMedia',
+    'editMessageLiveLocation',
+    'editMessageReplyMarkup',
+    'stopMessageLiveLocation',
+    'stopPoll'
+]
