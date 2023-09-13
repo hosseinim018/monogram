171f1d395df2622501e744af79fab89fd057d22c
mahdi hosseini <hosseinim018@gmail.com>
2023-09-13 06:32:39 +0330

    import and add Update class

--- telegram/bot/UpdatingMessages/__init__.py
+++ telegram/bot/UpdatingMessages/__init__.py
@@ -6,7 +6,7 @@ from stopPoll import stop_poll
 from editMessageLiveLocation import edit_message_live_location
 from editMessageMedia import edit_message_media
 from editMessageReplyMarkup import edit_message_reply_markup
-
+from Update import Update
 __All__ = [
     'deleteMessage',
     'editMessageCaption',
@@ -15,5 +15,6 @@ __All__ = [
     'editMessageLiveLocation',
     'editMessageReplyMarkup',
     'stopMessageLiveLocation',
-    'stopPoll'
+    'stopPoll',
+    'Update'
 ]
