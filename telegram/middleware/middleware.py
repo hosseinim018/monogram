46c232f6e4ac8839a7c1d2305fe4bf27323b7fcf
mahdi hosseini <hosseinim018@gmail.com>
2023-09-15 19:24:48 +0330

    update middleware

--- telegram/middleware/middleware.py
+++ telegram/middleware/middleware.py
@@ -1,4 +1,5 @@
 import json
+from telegram.bot.config import SECRET_TOKEN
 
 class MyMiddleware:
     def __init__(self, get_response):
@@ -7,11 +8,14 @@ class MyMiddleware:
     def __call__(self, request):
         # Perform your request checking logic here
         if request.method == 'POST':
-            result = json.loads(request.body.decode('utf-8'))
+            # result = json.loads(request.body.decode('utf-8'))
             headers = request.headers
             secret_token = headers['X-Telegram-Bot-Api-Secret-Token']
+            if secret_token == SECRET_TOKEN:
+                response = self.get_response(request)
+                # Perform any response processing here
+                # print('SECRET_TOKEN is valid')
+                return response
+            else:
+                raise Exception('SECRET_TOKEN is not valid')
 
-        response = self.get_response(request)
-        # Perform any response processing here
-
-        return response
