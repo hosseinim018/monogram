e90edc6ffe885f786f8a5fbf5c5d2d5ae2ffc5ca
mahdi hosseini <hosseinim018@gmail.com>
2023-09-15 16:49:03 +0330

    create Markup class

---
+++ telegram/bot/Buttons/Markup.py
@@ -0,0 +1,17 @@
+from typing import List
+from .Button import Inline
+
+class Markup:
+    def Inline(self, inline_keyboard: List[List[Inline]]) -> dict:
+        """
+        This mehtod represents an inline keyboard that appears right next to the message it belongs to.
+        Initialize an InlineKeyboardMarkup object.
+
+        :param inline_keyboard: The list of button rows in the inline keyboard.
+        :return Markup: dict.
+        """
+        Markup = {
+            'inline_keyboard': inline_keyboard
+        }
+        return Markup
+
