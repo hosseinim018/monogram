4b079a7274f15f5ced53ec1b4328c28ebb8d7305
mahdi hosseini <hosseinim018@gmail.com>
2023-09-16 22:37:17 +0330

    modified

--- telegram/bot/types/InlineKeyboardMarkup.py
+++ telegram/bot/types/InlineKeyboardMarkup.py
@@ -6,16 +6,15 @@ class InlineKeyboardMarkup:
     """
     This object represents an inline keyboard that appears right next to the message it belongs to.
     """
-    def __init__(self, inline_keyboard: List[List[InlineKeyboardButton]]):
+    def __new__(cls, inline_keyboard: List[List[InlineKeyboardButton]]) -> dict:
         """
         Initialize an InlineKeyboardMarkup object.
 
         :param inline_keyboard: The list of button rows in the inline keyboard.
         """
-        self.inline_keyboard = inline_keyboard
 
-    def call(self) -> dict:
         markup = {
-            'inline_keyboard': self.inline_keyboard
+            'inline_keyboard': inline_keyboard
         }
         return markup
+
