a5807d7d2ef194d80c50a8d4722b958d45331fef
mahdi hosseini <hosseinim018@gmail.com>
2023-09-17 18:09:14 +0330

    replace __new__ with __init__ and modified

--- telegram/bot/types/BotCommand.py
+++ telegram/bot/types/BotCommand.py
@@ -3,7 +3,7 @@ class BotCommand:
     Represents a bot command.
     """
 
-    def __init__(self, command: str, description: str):
+    def __new__(cls, command: str, description: str):
         """
         Initialize a BotCommand object.
 
@@ -11,5 +11,8 @@ class BotCommand:
             command (str): Text of the command; 1-32 characters. Can contain only lowercase English letters, digits, and underscores.
             description (str): Description of the command; 1-256 characters.
         """
-        self.command = command
-        self.description = description
+        cls.command = command
+        cls.description = description
+
+        payload = {"command": command, "description": description}
+        return payload
