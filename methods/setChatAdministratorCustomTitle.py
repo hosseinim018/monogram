7544e49834ce73523d4471eb4369ed50bd92c601
mahdi hosseini <hosseinim018@gmail.com>
2023-12-17 19:04:53 +0330

    update to class

--- methods/setChatAdministratorCustomTitle.py
+++ methods/setChatAdministratorCustomTitle.py
@@ -1,18 +1,22 @@
 from typing import Union
+from monogram import Monogram, validate_payload
 
-def set_chat_administrator_custom_title(
-    chat_id: Union[int, str],
-    user_id: int,
-    custom_title: str,
-) -> bool:
-    """
-    Use this method to set a custom title for an administrator in a supergroup promoted by the bot.
-    Returns True on success.
+class setChatAdministratorCustomTitle(Monogram):
+    def __new__(cls,
+        chat_id: Union[int, str],
+        user_id: int,
+        custom_title: str,
+    ) -> bool:
+        """
+        Use this method to set a custom title for an administrator in a supergroup promoted by the bot.
+        Returns True on success.
 
-    :param chat_id: Unique identifier for the target chat or username of the target supergroup (in the format @supergroupusername)
-    :param user_id: Unique identifier of the target user
-    :param custom_title: New custom title for the administrator; 0-16 characters, emoji are not allowed
-    :return: True on success
-    """
-    # Your implementation here
-    pass
+        :param chat_id: Unique identifier for the target chat or username of the target supergroup (in the format @supergroupusername)
+        :param user_id: Unique identifier of the target user
+        :param custom_title: New custom title for the administrator; 0-16 characters, emoji are not allowed
+        :return: True on success
+        """
+        payload = validate_payload(locals().copy())
+        # send post request to telegram based on method sendMessage, Construct the API endpoint URL
+        response = cls.request(cls, method='sendVideo', data=payload, res=True)
+        return response.json()
