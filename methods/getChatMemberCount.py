907a68bde6a43a51e2e90a05cbecf32e9c9529e6
mahdi hosseini <hosseinim018@gmail.com>
2023-12-17 19:59:58 +0330

    update to class

--- methods/getChatMemberCount.py
+++ methods/getChatMemberCount.py
@@ -1,12 +1,16 @@
 from typing import Union
+from monogram import Monogram, validate_payload
 
-def get_chat_member_count(chat_id: Union[int, str]) -> int:
-    """
-    Use this method to get the number of members in a chat.
-    Returns an int on success.
+class getChatMemberCount(Monogram):
+    def __new__(cls ,chat_id: Union[int, str]) -> int:
+        """
+        Use this method to get the number of members in a chat.
+        Returns an int on success.
 
-    :param chat_id: Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
-    :return: Number of members in the chat (int)
-    """
-    # Your implementation here
-    pass
+        :param chat_id: Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
+        :return: Number of members in the chat (int)
+        """
+        payload = validate_payload(locals().copy())
+        # send post request to telegram based on method sendMessage, Construct the API endpoint URL
+        response = cls.request(cls, method='getChatMemberCount', data=payload, res=True)
+        return response.json()
