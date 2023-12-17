834fb18b414d887add69ffa5e00bd885248e6835
mahdi hosseini <hosseinim018@gmail.com>
2023-12-17 18:51:38 +0330

    update to class

--- methods/sendContact.py
+++ methods/sendContact.py
@@ -1,28 +1,33 @@
 from typing import Union, Optional
+from monogram import Monogram, validate_payload
+from monogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ForceReply, Message
 
-def send_contact(chat_id: Union[int, str], phone_number: str, first_name: str, last_name: Optional[str] = None,
-                 message_thread_id: Optional[int] = None, vcard: Optional[str] = None,
-                 disable_notification: Optional[bool] = None, protect_content: Optional[bool] = None,
-                 reply_to_message_id: Optional[int] = None, allow_sending_without_reply: Optional[bool] = None,
-                 reply_markup: Optional[Union[InlineKeyboardMarkup, ReplyKeyboardMarkup,
-                                               ReplyKeyboardRemove, ForceReply]] = None) -> Message:
-    """
-    Use this method to send phone contacts. On success, the sent Message is returned.
+class sendContact(Monogram):
+    def __new__(cls, chat_id: Union[int, str], phone_number: str, first_name: str, last_name: Optional[str] = None,
+                     message_thread_id: Optional[int] = None, vcard: Optional[str] = None,
+                     disable_notification: Optional[bool] = None, protect_content: Optional[bool] = None,
+                     reply_to_message_id: Optional[int] = None, allow_sending_without_reply: Optional[bool] = None,
+                     reply_markup: Optional[Union[InlineKeyboardMarkup, ReplyKeyboardMarkup,
+                                                   ReplyKeyboardRemove, ForceReply]] = None) -> Message:
+        """
+        Use this method to send phone contacts. On success, the sent Message is returned.
 
-    :param chat_id: Unique identifier for the target chat or username of the target channel (in the format @channelusername)
-    :param phone_number: Contact's phone number
-    :param first_name: Contact's first name
-    :param last_name: Contact's last name
-    :param message_thread_id: Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
-    :param vcard: Additional data about the contact in the form of a vCard, 0-2048 bytes
-    :param disable_notification: Sends the message silently. Users will receive a notification with no sound.
-    :param protect_content: Protects the contents of the sent message from forwarding and saving
-    :param reply_to_message_id: If the message is a reply, ID of the original message
-    :param allow_sending_without_reply: Pass True if the message should be sent even if the specified replied-to message is not found
-    :param reply_markup: Additional interface options.
-                         A JSON-serialized object for an inline keyboard, custom reply keyboard,
-                         instructions to remove reply keyboard or to force a reply from the user.
-    :return: The sent Message object
-    """
-    # Your implementation here
-    pass
+        :param chat_id: Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+        :param phone_number: Contact's phone number
+        :param first_name: Contact's first name
+        :param last_name: Contact's last name
+        :param message_thread_id: Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
+        :param vcard: Additional data about the contact in the form of a vCard, 0-2048 bytes
+        :param disable_notification: Sends the message silently. Users will receive a notification with no sound.
+        :param protect_content: Protects the contents of the sent message from forwarding and saving
+        :param reply_to_message_id: If the message is a reply, ID of the original message
+        :param allow_sending_without_reply: Pass True if the message should be sent even if the specified replied-to message is not found
+        :param reply_markup: Additional interface options.
+                             A JSON-serialized object for an inline keyboard, custom reply keyboard,
+                             instructions to remove reply keyboard or to force a reply from the user.
+        :return: The sent Message object
+        """
+        payload = validate_payload(locals().copy())
+        # send post request to telegram based on method sendMessage, Construct the API endpoint URL
+        response = cls.request(cls, method='sendVideo', data=payload, res=True)
+        return response.json()
