9300218d6ee899f4257d62a6d62a1efcad37a3c8
mahdi hosseini <hosseinim018@gmail.com>
2023-12-17 20:04:04 +0330

    update to class

--- methods/createForumTopic.py
+++ methods/createForumTopic.py
@@ -1,14 +1,19 @@
-def create_forum_topic(chat_id, name, icon_color=None, icon_custom_emoji_id=None):
-    """
-    Use this method to create a topic in a forum supergroup chat.
-    The bot must be an administrator in the chat for this to work and must have the can_manage_topics administrator rights.
-    Returns information about the created topic as a ForumTopic object.
+from monogram import Monogram, validate_payload
 
-    :param chat_id: Unique identifier for the target chat or username of the target supergroup (in the format @supergroupusername)
-    :param name: Topic name, 1-128 characters
-    :param icon_color: Color of the topic icon in RGB format. Currently, must be one of the allowed values
-    :param icon_custom_emoji_id: Unique identifier of the custom emoji shown as the topic icon
-    :return: Information about the created topic as a ForumTopic object
-    """
-    # Your implementation here
-    pass
+class createForumTopic(Monogram):
+    def __new__(cls, chat_id, name, icon_color=None, icon_custom_emoji_id=None):
+        """
+        Use this method to create a topic in a forum supergroup chat.
+        The bot must be an administrator in the chat for this to work and must have the can_manage_topics administrator rights.
+        Returns information about the created topic as a ForumTopic object.
+
+        :param chat_id: Unique identifier for the target chat or username of the target supergroup (in the format @supergroupusername)
+        :param name: Topic name, 1-128 characters
+        :param icon_color: Color of the topic icon in RGB format. Currently, must be one of the allowed values
+        :param icon_custom_emoji_id: Unique identifier of the custom emoji shown as the topic icon
+        :return: Information about the created topic as a ForumTopic object
+        """
+        payload = validate_payload(locals().copy())
+        # send post request to telegram based on method sendMessage, Construct the API endpoint URL
+        response = cls.request(cls, method='createForumTopic', data=payload, res=True)
+        return response.json()
