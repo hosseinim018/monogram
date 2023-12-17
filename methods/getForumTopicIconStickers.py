975cfdc28c63dede1e44b19f2b0b39fd91af4024
mahdi hosseini <hosseinim018@gmail.com>
2023-12-17 20:03:19 +0330

    update to class

--- methods/getForumTopicIconStickers.py
+++ methods/getForumTopicIconStickers.py
@@ -1,10 +1,15 @@
-def get_forum_topic_icon_stickers():
-    """
-    Use this method to get custom emoji stickers that can be used as a forum topic icon by any user.
-    Requires no parameters.
-    Returns an array of Sticker objects.
+from monogram import Monogram, validate_payload
 
-    :return: Array of Sticker objects
-    """
-    # Your implementation here
-    pass
+class getForumTopicIconStickers(Monogram):
+    def __new__(cls):
+        """
+        Use this method to get custom emoji stickers that can be used as a forum topic icon by any user.
+        Requires no parameters.
+        Returns an array of Sticker objects.
+
+        :return: Array of Sticker objects
+        """
+        payload = validate_payload(locals().copy())
+        # send post request to telegram based on method sendMessage, Construct the API endpoint URL
+        response = cls.request(cls, method='getForumTopicIconStickers', data=payload, res=True)
+        return response.json()
