c1f111616c5108a8df3f17ff71c8bfe278e57b39
mahdi hosseini <hosseinim018@gmail.com>
2023-12-17 18:55:04 +0330

    update to class

--- methods/sendDice.py
+++ methods/sendDice.py
@@ -1,27 +1,32 @@
 from typing import Union, Optional
+from monogram import Monogram, validate_payload
+from monogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ForceReply, Message
 
-def send_dice(chat_id: Union[int, str], emoji: Optional[str] = "🎲",
-              message_thread_id: Optional[int] = None, disable_notification: Optional[bool] = None,
-              protect_content: Optional[bool] = None, reply_to_message_id: Optional[int] = None,
-              allow_sending_without_reply: Optional[bool] = None,
-              reply_markup: Optional[Union[InlineKeyboardMarkup, ReplyKeyboardMarkup,
-                                            ReplyKeyboardRemove, ForceReply]] = None) -> Message:
-    """
-    Use this method to send an animated emoji that will display a random value. On success, the sent Message is returned.
+class sendDice(Monogram):
+    def __new__(cls, chat_id: Union[int, str], emoji: Optional[str] = "🎲",
+                  message_thread_id: Optional[int] = None, disable_notification: Optional[bool] = None,
+                  protect_content: Optional[bool] = None, reply_to_message_id: Optional[int] = None,
+                  allow_sending_without_reply: Optional[bool] = None,
+                  reply_markup: Optional[Union[InlineKeyboardMarkup, ReplyKeyboardMarkup,
+                                                ReplyKeyboardRemove, ForceReply]] = None) -> Message:
+        """
+        Use this method to send an animated emoji that will display a random value. On success, the sent Message is returned.
 
-    :param chat_id: Unique identifier for the target chat or username of the target channel (in the format @channelusername)
-    :param emoji: Emoji on which the dice throw animation is based. Currently, must be one of “🎲”, “🎯”, “🏀”, “⚽”, “🎳”, or “🎰”.
-                  Dice can have values 1-6 for “🎲”, “🎯” and “🎳”, values 1-5 for “🏀” and “⚽”, and values 1-64 for “🎰”.
-                  Defaults to “🎲”
-    :param message_thread_id: Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
-    :param disable_notification: Sends the message silently. Users will receive a notification with no sound.
-    :param protect_content: Protects the contents of the sent message from forwarding
-    :param reply_to_message_id: If the message is a reply, ID of the original message
-    :param allow_sending_without_reply: Pass True if the message should be sent even if the specified replied-to message is not found
-    :param reply_markup: Additional interface options.
-                         A JSON-serialized object for an inline keyboard, custom reply keyboard,
-                         instructions to remove reply keyboard or to force a reply from the user.
-    :return: The sent Message object
-    """
-    # Your implementation here
-    pass
+        :param chat_id: Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+        :param emoji: Emoji on which the dice throw animation is based. Currently, must be one of “🎲”, “🎯”, “🏀”, “⚽”, “🎳”, or “🎰”.
+                      Dice can have values 1-6 for “🎲”, “🎯” and “🎳”, values 1-5 for “🏀” and “⚽”, and values 1-64 for “🎰”.
+                      Defaults to “🎲”
+        :param message_thread_id: Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
+        :param disable_notification: Sends the message silently. Users will receive a notification with no sound.
+        :param protect_content: Protects the contents of the sent message from forwarding
+        :param reply_to_message_id: If the message is a reply, ID of the original message
+        :param allow_sending_without_reply: Pass True if the message should be sent even if the specified replied-to message is not found
+        :param reply_markup: Additional interface options.
+                             A JSON-serialized object for an inline keyboard, custom reply keyboard,
+                             instructions to remove reply keyboard or to force a reply from the user.
+        :return: The sent Message object
+        """
+        payload = validate_payload(locals().copy())
+        # send post request to telegram based on method sendMessage, Construct the API endpoint URL
+        response = cls.request(cls, method='sendDice', data=payload, res=True)
+        return response.json()
