7767529ffa11c560fda6713ac1324415d253c14d
mahdi hosseini <hosseinim018@gmail.com>
2023-12-17 20:10:03 +0330

    update to class

--- methods/closeGeneralForumTopic.py
+++ methods/closeGeneralForumTopic.py
@@ -1,11 +1,16 @@
-def close_general_forum_topic(chat_id):
-    """
-    Use this method to close an open 'General' topic in a forum supergroup chat.
-    The bot must be an administrator in the chat for this to work and must have the can_manage_topics administrator rights.
-    Returns True on success.
+from monogram import Monogram, validate_payload
 
-    :param chat_id: Unique identifier for the target chat or username of the target supergroup (in the format @supergroupusername)
-    :return: True on success
-    """
-    # Your implementation here
-    pass
+class closeGeneralForumTopic(Monogram):
+    def __new__(cls, chat_id):
+        """
+        Use this method to close an open 'General' topic in a forum supergroup chat.
+        The bot must be an administrator in the chat for this to work and must have the can_manage_topics administrator rights.
+        Returns True on success.
+
+        :param chat_id: Unique identifier for the target chat or username of the target supergroup (in the format @supergroupusername)
+        :return: True on success
+        """
+        payload = validate_payload(locals().copy())
+        # send post request to telegram based on method sendMessage, Construct the API endpoint URL
+        response = cls.request(cls, method='closeGeneralForumTopic', data=payload, res=True)
+        return response.json()
