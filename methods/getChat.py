fd68b6591131d2dea393c391f983dab335e603a0
mahdi hosseini <hosseinim018@gmail.com>
2023-12-17 19:58:08 +0330

    update to class

--- methods/getChat.py
+++ methods/getChat.py
@@ -1,13 +1,17 @@
 from typing import Union
+from monogram import Monogram, validate_payload
 
-def get_chat(chat_id: Union[int, str]) -> dict:
-    """
-    Use this method to get up-to-date information about the chat (current name of the user for one-on-one conversations,
-    current username of a user, group or channel, etc.).
-    Returns a Chat object on success.
+class getChat(Monogram):
+    def __new__(cls, chat_id: Union[int, str]) -> dict:
+        """
+        Use this method to get up-to-date information about the chat (current name of the user for one-on-one conversations,
+        current username of a user, group or channel, etc.).
+        Returns a Chat object on success.
 
-    :param chat_id: Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
-    :return: Chat object on success
-    """
-    # Your implementation here
-    pass
+        :param chat_id: Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
+        :return: Chat object on success
+        """
+        payload = validate_payload(locals().copy())
+        # send post request to telegram based on method sendMessage, Construct the API endpoint URL
+        response = cls.request(cls, method='getChat', data=payload, res=True)
+        return response.json()
